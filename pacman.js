f1202b29e814495ebcbed1f8c3fedac5a5e91e94
Tomasz Flakus <t.flakus@swce.pl>
2020-08-11 08:46:01 +0200

    add moves

--- pacman.js
+++ pacman.js
@@ -820,9 +820,58 @@ document.addEventListener("DOMContentLoaded", () => {
   createBoard();
 
   //starting position of pac-man
-  let pacmanCurentPosition = 490;
-  squares[pacmanCurentPosition].classList.add("pac-man");
+  let pacmanCurrentIndex = 490;
+  squares[pacmanCurrentIndex].classList.add("pac-man");
 
   //move pacman
+  function movePacman(e) {
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+    switch(e.keyCode) {
+      case 37:
+        if(
+          pacmanCurrentIndex % width !== 0 &&
+          !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')
+          )
+        pacmanCurrentIndex -= 1
+        if (squares[pacmanCurrentIndex -1] === squares[363]) {
+          pacmanCurrentIndex = 391
+        }
+        break
+      case 38:
+        if(
+          pacmanCurrentIndex - width >= 0 &&
+          !squares[pacmanCurrentIndex -width].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')
+          ) 
+        pacmanCurrentIndex -= width
+        break
+      case 39:
+        if(
+          pacmanCurrentIndex % width < width - 1 &&
+          !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')
+        )
+        pacmanCurrentIndex += 1
+        if (squares[pacmanCurrentIndex +1] === squares[392]) {
+          pacmanCurrentIndex = 364
+        }
+        break
+      case 40:
+        if (
+          pacmanCurrentIndex + width < width * width &&
+          !squares[pacmanCurrentIndex +width].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')
+        )
+        pacmanCurrentIndex += width
+        break
+    }
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+    // pacDotEaten()
+    // powerPelletEaten()
+    // checkForGameOver()
+    // checkForWin()
+  }
+  document.addEventListener("keyup", movePacman);
 });
-document.addEventListener("keyup", movePacman);
+
